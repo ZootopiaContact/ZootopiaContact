88b085141bd8b3a09e63d879717732f685438ab5
김진아 <c2010333@naver.com>
2023-09-04 15:59:03 +0900

    [Feat]:zootopiaInfo

---
+++ app/src/main/java/com/example/zootopiapage/ZootopiaInfo.kt
@@ -0,0 +1,10 @@
+package com.example.zootopiapage
+
+data class ZootopiaInfo(
+    val profile : Int,
+    val name : String,
+    val birth : String,
+    val call : String,
+    val like : String,
+    val hobby : String
+)
