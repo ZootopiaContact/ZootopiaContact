6811292895fe9ccda9a45bc9954396ddacf5ee67
kwonyongil <kkwonyi95@gmail.com>
2023-09-09 20:44:42 +0900

    확인용

--- app/src/main/java/com/example/zootopiapage/MainActivity.kt
+++ app/src/main/java/com/example/zootopiapage/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.zootopiapage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.viewpager2.widget.ViewPager2
 import com.example.zootopiapage.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         viewPager2Adapter = ViewPager2Adapter(this)
         viewPager2Adapter.addFragment(ContactListFragment.newInstance("param1", "param2"))
         viewPager2Adapter.addFragment(MypageFragment())
+        viewPager2Adapter.addFragment(ContactDetailFragment())
 
         initViewPager()
 
@@ -39,18 +41,10 @@ class MainActivity : AppCompatActivity() {
             when (position) {
                 0 -> tab.text = "연락처"
                 1 -> tab.text = "마이 페이지"
+                else -> tab.view.visibility = View.GONE
             }
         }.attach()
-
-        /*registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-            }
-        })
-    }*/
-
     }
-
     fun onRecyclerViewItemClick(position: Int, contactData: ZootopiaInfo) {
         val fragmentB = viewPager2Adapter.getFragment(2) as? ContactDetailFragment
         if (fragmentB != null) {
